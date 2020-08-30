2e493bcc4e96a06408b11701e7f6e0b988409774
khoffy <khoffy.tipoh@gmail.com>
2020-08-30 21:03:15 +0000

    Initialisation du projet movieapp pour la presenttion des tendances de films

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  ApiConstants._();   // private constructor
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "76dacd54eaa5157a3c16b99e42ed5b26";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+  
+}
